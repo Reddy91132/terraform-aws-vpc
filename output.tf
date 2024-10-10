3bf91de35ac6d467bbaa899890e0e893d2b33c15
yuvasai07 <reddy123hon85@gmail.com>
2024-10-10 11:43:42 +0530

    terraform

--- output.tf
+++ output.tf
@@ -1,6 +1,21 @@
-output "vpc_id" {
+output  "vpc_id" {
   value       = aws_vpc.main.id
+}
 
+output "public_subnet_ids"{
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids"{
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids"{
+  value = aws_subnet.database[*].id
+}
+
+output "database_subnet_group_name"{
+  value = aws_db_subnet_group.default.name
 }
 
 output "az_info" {
@@ -8,5 +23,9 @@ output "az_info" {
 }
 
 output "default_vpc_info" {
-  value = data.aws_vpc.default 
+  value = data.aws_vpc.default
+}
+
+output "main_route_table_info" {
+  value = data.aws_route_table.main
 }
